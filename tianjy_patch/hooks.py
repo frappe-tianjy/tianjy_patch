dcfbd2840a9f1a3f2d5403ba00493e1d2f738b0c
Fierflame <git@fierflame.com>
2023-11-28 17:59:13 +0800

    新增：将前端脚本加入 app 页面

--- tianjy_patch/hooks.py
+++ tianjy_patch/hooks.py
@@ -7,12 +7,12 @@ app_description = "Tianjy Patch"
 app_email = "Tianjy"
 app_license = "MIT"
 
-# Includes in <head>
-# ------------------
-
 # include js, css files in header of desk.html
 # app_include_css = "/assets/tianjy_patch/css/tianjy_patch.css"
-# app_include_js = "/assets/tianjy_patch/js/tianjy_patch.js"
+app_include_js = "tianjy_patch.bundle.js"
+
+# Includes in <head>
+# ------------------
 
 # include js, css files in header of web template
 # web_include_css = "/assets/tianjy_patch/css/tianjy_patch.css"
